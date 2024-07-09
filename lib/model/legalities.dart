7bb78f6ecba2883be80f966f21e859185f01b928
Thomas Werner <thomas.werner89@gmail.com>
2024-07-09 22:58:12 +0200

    -removed unnessecary fields in legalities ToDo: -upsert even when db is dropped -Keywords to look for: partner, partner with, friends forever, choose a Background, Doctor's companion, companion (set:iko),

--- lib/model/legalities.dart
+++ lib/model/legalities.dart
@@ -1,99 +1,51 @@
 class Legalities {
   final String standard;
-  final String future;
-  final String historic;
-  final String gladiator;
-  final String pioneer;
-  final String explorer;
   final String modern;
   final String legacy;
   final String pauper;
-  final String vintage;
-  final String penny;
   final String commander;
   final String oathbreaker;
   final String brawl;
-  final String standardBrawl;
-  final String alchemy;
   final String pauperCommander;
   final String duel;
-  final String oldSchool;
-  final String preModern;
-  final String preDH;
 
   Legalities({
     required this.standard,
-    required this.future,
-    required this.historic,
-    required this.gladiator,
-    required this.pioneer,
-    required this.explorer,
     required this.modern,
     required this.legacy,
     required this.pauper,
-    required this.vintage,
-    required this.penny,
     required this.commander,
     required this.oathbreaker,
     required this.brawl,
-    required this.standardBrawl,
-    required this.alchemy,
     required this.pauperCommander,
     required this.duel,
-    required this.oldSchool,
-    required this.preModern,
-    required this.preDH,
   });
 
   factory Legalities.fromJson(Map<String, dynamic> json) {
     return Legalities(
       standard: json['standard'],
-      future: json['future'],
-      historic: json['historic'],
-      gladiator: json['gladiator'],
-      pioneer: json['pioneer'],
-      explorer: json['explorer'],
       modern: json['modern'],
       legacy: json['legacy'],
       pauper: json['pauper'],
-      vintage: json['vintage'],
-      penny: json['penny'],
       commander: json['commander'],
       oathbreaker: json['oathbreaker'],
       brawl: json['brawl'],
-      standardBrawl: json['standardbrawl'],
-      alchemy: json['alchemy'],
       pauperCommander: json['paupercommander'],
       duel: json['duel'],
-      oldSchool: json['oldschool'],
-      preModern: json['premodern'],
-      preDH: json['predh'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'standard': standard,
-      'future': future,
-      'historic': historic,
-      'gladiator': gladiator,
-      'pioneer': pioneer,
-      'explorer': explorer,
       'modern': modern,
       'legacy': legacy,
       'pauper': pauper,
-      'vintage': vintage,
-      'penny': penny,
       'commander': commander,
       'oathbreaker': oathbreaker,
       'brawl': brawl,
-      'standardbrawl': standardBrawl,
-      'alchemy': alchemy,
       'paupercommander': pauperCommander,
       'duel': duel,
-      'oldschool': oldSchool,
-      'premodern': preModern,
-      'predh': preDH,
     };
   }
 }
